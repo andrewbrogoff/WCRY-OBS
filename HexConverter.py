0023c92b4ad3f20b0a6cf429ccb3ed831d66e6d5
bookbot2019 <46849241+bookbot2019@users.noreply.github.com>
2022-02-12 13:47:43 -0500

    Cleaning up the page

--- HexConverter.py
+++
@@ -1,6 +0,0 @@
-import binascii
-filename = 'Malware.zip'
-with open(filename, 'rb') as f:
-    content = f.read()
-with open('hexData.hex', 'wb') as hexfile:
-    hexfile.write(binascii.hexlify(content))
--- ImagetoAscii.py
+++
@@ -1,5 +0,0 @@
-import pywhatkit as kt
-print("Let's turn images to ASCII art!")
-source_path = 'Oshawott.png'
-target_path = 'Oshawott.txt'
-kt.image_to_ascii_art(source_path, target_path)
